66f705ab9323b5a240bdbfb7a9e9282246a1f724
Roman Vyazkov <Cheetos-12@yandex.com>
2026-07-12 17:26:16 +0300

    Исправлено

--- fit_life.py
+++ fit_life.py
@@ -1,37 +1,34 @@
-# Проект FitLife - MVP версия 1.0
-
+""" Проект FitLife - MVP версия 1.0 """
+water_per_kg = 30
+volume_water_ml = 1000
 
 # запрос имени
 user_name = input('Введите своё имя:')
 
 
 # запрос возраста
-user_age = input('Введите свой возраст:')
-user_age_int = int(user_age)
+user_age = int(input('Введите свой возраст:'))
 
 
 # запрос веса
-user_weght = input('Введите свой вес:')
-user_weght_float = float(user_weght)
+user_weght = float(input('Введите свой вес:'))
 
 
 # запрос роста
-user_height = input('Введите свой рост в метрах, (например: 1.75):')
-user_height_float = float(user_height)
+user_height = float(input('Введите свой рост в метрах, (например: 1.75):'))
 
 
 # расчёт индекса массы тела
-bmi = user_weght_float / (user_height_float ** 2)
+bmi = user_weght / (user_height ** 2)
 round(bmi, 1)
 
 
 # расчёт нормы воды в литрах
-water_per_kg = 30
-water_ml = user_weght_float * water_per_kg
-water_l = water_ml / 1000
+water_ml = user_weght * water_per_kg
+water_l = water_ml / volume_water_ml
 
 # вывод результата с помощью f-строк
-print(f'Привет, {user_name} {user_age_int} лет !')
+print(f'Привет, {user_name}!\nТебе {user_age} лет')
 print(f'Твой индекс массы тела: {bmi:.1f} кг/м²')
 print(f'Рекомендуемая норма воды: {water_l:.1f} л. в день')
 print("Расчет окончен. Будьте здоровы!")
